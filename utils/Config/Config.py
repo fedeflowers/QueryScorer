88cef6df4ef2028cc1e3610e0ab82e16e4aa6a2a
Federico Fiorio <f.fiorio@reply.it>
2025-09-17 18:02:21 +0200

    postgresql

--- utils/Config/Config.py
+++ utils/Config/Config.py
@@ -8,8 +8,8 @@ class Config:
     POSTGRES_DBNAME = os.getenv("POSTGRES_DBNAME", "scorer")
     POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-    # POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres") # when running on github actions
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    # POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres") # when running on github actions
     POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", 5432))
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
     MONGO_DBNAME = os.getenv("MONGO_DBNAME", "scorer")
